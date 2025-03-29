cccdbb4908578792255e5975415feec9696af5c5
RajeevRanjan73 <RajeevRanjan73@IBM-PF32P2P0>
2025-03-29 09:37:21 +0530

    commit new class

---
+++ src/test/java/com/selenium/practise/Branch2Test.java
@@ -0,0 +1,11 @@
+package com.selenium.practise;
+
+public class Branch2Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi there");
+
+	}
+
+}
